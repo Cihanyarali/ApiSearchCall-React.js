5dad14afdcec08e75bc9c56d4101309522b6a0f0
Cihan Yaralı <109430346+Cihanyarali@users.noreply.github.com>
2023-03-21 12:03:58 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import SearchHeader from "./SearchHeader";
+import searchImages from "./components/api";
+import { useState } from "react";
+import ImageList from "./components/ImageList";
+
+function App() {
+  const [images, setImages] = useState([]);
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term);
+    setImages(result);
+  };
+  return (
+    <div className="App">
+      <SearchHeader search={handleSubmit} />
+      <ImageList imagePlaceHolder={images} />
+    </div>
+  );
+}
+
+export default App;
